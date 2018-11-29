161ce25a8289ad1c0a5634ad03fb29a5acea7d9f
Дмитрий Маскаев <d.maskaev@infomaximum.com>
2018-11-29 10:53:07 +0300

    feat: добавил функциональный компонент

---
+++ src/components/stateless/GreeterFunc/GreeterFunc.jsx
@@ -0,0 +1,2 @@
+import React from "react";
+export const GreeterFunc = props => <h1 className="h1-border">{props.greeting}</h1>;
--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { Greeter } from "../../components/stateless/Greeter/Greeter";
+import { GreeterFunc } from "../../components/stateless/GreeterFunc/GreeterFunc";
 import { TextHideShow } from "../../components/stateful/TextHideShow/TextHideShow";
 
 class App extends Component {
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div>
         <Greeter greeting="Рендерим текст через stateless компонент" />
+        <GreeterFunc greeting="Рендерим текст через функциональный stateless компонент" />
         <TextHideShow text="Рендерим текст через stateful компонент" />
       </div>
     );
